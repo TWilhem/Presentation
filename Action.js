2f4ff40c8a89663225ffbe9975861ef326f872ea
TWilhem <tom.wilhem@etu.umontpellier.fr>
2024-09-18 15:53:45 +0200

    ll

--- Action.js
+++ Action.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.resize-image');
     const links = [
-        '',
-        '',
+        'https://twilhem.github.io/Presentation/Chess/echecs.html',
+        'https://twilhem.github.io/Presentation/Chess/echecs.html',
         'https://twilhem.github.io/Presentation/Chess/echecs.html'
     ];
 
